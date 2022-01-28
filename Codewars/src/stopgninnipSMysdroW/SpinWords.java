c507e67c30ba46a6cf76b1ee8476fbf3fa470395
pas8 <peterpreneur@gmail.com>
2022-01-28 01:05:55 -0600

    init files

---
+++ Codewars/src/stopgninnipSMysdroW/SpinWords.java
@@ -0,0 +1,12 @@
+package stopgninnipSMysdroW;
+
+public class SpinWords {
+
+	//public static void main(String[] args) {
+	
+	public String spinWords(String sentence) {
+		return sentence;
+		
+
+	}
+}
