ec5151f2f1f27a1ba7a810d4c4a4269994b6afd6
pas8 <peterpreneur@gmail.com>
2022-01-30 15:14:36 -0600

    test

--- Codewars/src/stopgninnipSMysdroW/SpinWords.java
+++ Codewars/src/stopgninnipSMysdroW/SpinWords.java
@@ -2,11 +2,26 @@ package stopgninnipSMysdroW;
 
 public class SpinWords {
 
-	//public static void main(String[] args) {
-	
-	public String spinWords(String sentence) {
-		return sentence;
-		
+	public static void main(String[] args) {
+		// public String spinWords(String sentence) {
+		String sentence = "Dot saw I was Tod";
+		String words[] = sentence.split(" ");
+		for (String token : words) {
+			if (token.length() > 5) {
+			String word = token;			
+			}
+			
+			String test= spinWordGtFive(sentence);
 
+		}
+		// return spinWordGtFive(sentence);
+	}
+
+	private String spinWordGtFive(String word) {
+		StringBuilder sb = new StringBuilder(word);
+
+		String reverseWord = sb.reverse().toString(); // reverse it
+
+		return reverseWord;
 	}
 }
