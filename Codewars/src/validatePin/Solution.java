7063e13e2cc610048e6ccea7db3977e692cd40b2
pas8 <peterpreneur@gmail.com>
2022-02-03 08:44:56 -0600

    done

--- Codewars/src/validatePin/Solution.java
+++ Codewars/src/validatePin/Solution.java
@@ -1,12 +1,21 @@
 package validatePin;
 
 public class Solution {
+	
+	public static boolean validatePin(String pin) {
+	// public static void main(String[] args) {
+	// String pin = "123456";
+		if ((pin.length() == 4) || (pin.length() == 6)) {
+			String text = "\\d+";
 
-  public static boolean validatePin(String pin) {
-    // Your code here...
-  if (pin.length=4 || pin.matches("\\d+\\d+\\d+\\d+")){
-      return true;
-    }
-  }
-
+			if (pin.matches(text.repeat(pin.length()))) {
+				// System.out.print(pin.length() + " true");
+				return true;
+			} else {
+				// System.out.print(pin.length() + " false");
+				return false;
+			}
+		}
+		return false;
+	}
 }
