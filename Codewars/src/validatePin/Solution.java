fe50385b317e0c9f2d8d583d9c08982e0862f836
pas8 <peterpreneur@gmail.com>
2022-02-03 08:14:42 -0600

    setup

---
+++ Codewars/src/validatePin/Solution.java
@@ -0,0 +1,12 @@
+package validatePin;
+
+public class Solution {
+
+  public static boolean validatePin(String pin) {
+    // Your code here...
+  if (pin.length=4 || pin.matches("\\d+\\d+\\d+\\d+")){
+      return true;
+    }
+  }
+
+}
