de0a4b7877483830a6ba89c3e3b86ca95b4ee3ca
pas8 <peterpreneur@gmail.com>
2022-02-03 09:17:15 -0600

    setup

---
+++ Codewars/src/findOdd/FindOdd.java
@@ -0,0 +1,15 @@
+package findOdd;
+
+public class FindOdd {
+
+	public static void main(String[] args) {
+	//public static int findIt(int[] a) {
+		int[]a= {10};
+		
+		System.out.print(a.toString());
+		
+		//if(a % 2 = 1){
+		  //  return odd;
+		  	//}
+	}
+}
