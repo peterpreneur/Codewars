aa18e8bd7fefbf015211655d14960c7ec37225c9
pas8 <peterpreneur@gmail.com>
2022-02-03 01:20:41 -0600

    update

--- Codewars/src/stopgninnipSMysdroW/SpinWordsBestTest.java
+++ Codewars/src/stopgninnipSMysdroW/SpinWordsBestTest.java
@@ -8,6 +8,7 @@ public class SpinWordsBestTest {
 	public void test() {
 		assertEquals("emocleW", new SpinWordsBest().spinWords("Welcome"));
 		assertEquals("Hey wollef sroirraw", new SpinWordsBest().spinWords("Hey fellow warriors"));
+		assertEquals("Just gniddik ereht[ is ]llits one more", new SpinWordsBest().spinWords("Just kidding [there is still] one more"));
 	}
 
 }
