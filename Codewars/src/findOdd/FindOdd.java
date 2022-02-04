4d0a84ab6ad7f1491fbf31ea21fb543ce09e7d1b
pas8 <peterpreneur@gmail.com>
2022-02-03 19:05:21 -0600

    findOdd

--- Codewars/src/findOdd/FindOdd.java
+++ Codewars/src/findOdd/FindOdd.java
@@ -1,15 +1,31 @@
 package findOdd;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FindOdd {
 
-	public static void main(String[] args) {
-	//public static int findIt(int[] a) {
-		int[]a= {10};
+	public static int findIt(int[] arrInt) {
+		//public static void main(String[] args) {
+		//int[] arrInt = { 5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10 };
 		
-		System.out.print(a.toString());
-		
-		//if(a % 2 = 1){
-		  //  return odd;
-		  	//}
+        // Create an empty List
+		List<Integer> arrIntList = new ArrayList<>();
+  
+        // Iterate through the array
+        for (int i : arrInt) {
+            // Add each element into the list
+        	arrIntList.add(i);
+        }
+  
+        for(int i = 0; i < arrInt.length; i++) {
+            int count = Collections.frequency(arrIntList, arrInt[i]);	
+            if (count %2 == 1 ) {
+            	//System.out.println(count + " is " + arrInt[i]);
+            	return arrInt[i];
+            } 
+        }
+		return 0;
 	}
 }
