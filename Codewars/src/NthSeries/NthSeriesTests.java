6fb22fdfeeef3b261a4932fb39bfb032a65c241c
pas8 <peterpreneur@gmail.com>
2022-02-07 10:20:27 -0600

    intro

---
+++ Codewars/src/NthSeries/NthSeriesTests.java
@@ -0,0 +1,24 @@
+package NthSeries;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+
+public class NthSeriesTests {
+
+	@Test
+	public void test1() {
+		assertEquals("1.57", NthSeries.seriesSum(5));
+	}
+	@Test
+	public void test2() {
+		assertEquals("1.77", NthSeries.seriesSum(9));
+	}
+	@Test
+	public void test3() {
+		assertEquals("1.94", NthSeries.seriesSum(15));
+	}
+}
