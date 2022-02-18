3e44016a538808ec9c2771f8d9207f9829889698
pas8 <peterpreneur@gmail.com>
2022-02-18 00:02:37 -0600

    stringToCamelCase

--- Codewars/src/stringToCamelCase/Solution1.java
+++ Codewars/src/stringToCamelCase/Solution1.java
@@ -5,12 +5,25 @@ import java.util.regex.Pattern;
 
 public class Solution1 {
 	static String toCamelCase(String s) {
-		Matcher m = Pattern.compile("[_|-](\\w)").matcher(s);
+		//public static void main(String[] args) {
+		// public static Pattern compile(String regex)
+		//String s = "the_stealth_warrior";
+		//System.out.println(s);
+		
+		String regexEx = "[_|-](\\w)";
+		Pattern pattern = Pattern.compile(regexEx);
+		//System.out.println(pattern);
+
+		Matcher m = pattern.matcher(s);
 		
 		StringBuffer sb = new StringBuffer();
+		//System.out.println(sb);	
 		while (m.find()) {
 			m.appendReplacement(sb, m.group(1).toUpperCase());
+			//System.out.println(m.toString());	
+			//System.out.println(sb);	
 		}
+		//System.out.println(m.appendTail(sb).toString());
 		return m.appendTail(sb).toString();
 	}
 }
